439f98ecedc846179602764cb6a4e4ff4c3b1e57
TPphoenix <duxiongweimail@163.com>
2019-07-13 15:54:11 +0800

    new file:   spiders/__init__.py 	new file:   spiders/__pycache__/__init__.cpython-37.pyc 	new file:   spiders/__pycache__/douban_spider.cpython-37.pyc 	new file:   spiders/douban_spider.py 	new file:   spiders/scrapy 	new file:   spiders/test.csv 	.idea/ 	__init__.py 	__pycache__/

---
+++ spiders/douban_spider.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+from douban.items import DoubanItem
+
+
+class DoubanSpiderSpider(scrapy.Spider):
+    # 爬虫名字
+    name = 'douban_spider'
+    # 允许域名
+    allowed_domains = ['movie.douban.com']
+    # 入口url扔到调度器里面去
+    start_urls = ['https://movie.douban.com/top250']
+
+    def parse(self, response):
+        movie_list = response.xpath("//div[@class='article']//ol[@class='grid_view']//li")
+        # 循环获取电影条目
+        for i_item in movie_list:
+            douban_item = DoubanItem()
+            # 写详细的xpath，进行数据的解析
+            douban_item['serial_number'] = i_item.xpath(".//div[@class='item']//em/text()").extract_first()
+            douban_item['movie_name'] = i_item.xpath(".//div[@class='item']//div[@class='info']//div[@class='hd']//span[1]/text()").extract_first()
+            content = i_item.xpath(".//div[@class='item']//div[@class='info']//div[@class='bd']//p[1]/text()").extract()
+            # 数据的处理
+            for i_content in content:
+                content_s = "".join(i_content.split())
+                douban_item['introduce'] = content_s
+            douban_item['star'] = i_item.xpath(".//span[@class='rating_num']/text()").extract_first()
+            douban_item['evaluate'] = i_item.xpath(".//div[@class='star']//span[4]/text()").extract_first()
+            douban_item['describe'] = i_item.xpath(".//p[@class='quote']//span/text()").extract_first()
+            # 将数据yield传到pipelines去，进行数据清洗
+            yield douban_item
+        # 解析下一页规则，取的后面的xpath
+        next_link = response.xpath("//span[@class='next']//link/@href").extract()
+        if next_link:
+            next_link = next_link[0]
+            yield scrapy.Request("https://movie.douban.com/top250"+next_link, callback=self.parse)
+
+
